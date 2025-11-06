00622cd4532dd6fc08b2b12ddf2c4e931eee08d0
Oliver Trojanowski <trojanowskioliver1@gmail.com>
2025-11-06 20:20:58 +0100

    Add getter methods to MapData interface classes

--- Viewer/lib/i_map_data.dart
+++ Viewer/lib/i_map_data.dart
@@ -1,22 +1,44 @@
 abstract class IMapData {
   IMapData(String mapName, String mapVersion, Map<String, IBuilding> buildings, List<INode> nodes, List<IEdge> edges);
-  Map<String, IBuilding> getBuildingNames();
+
+  String getMapName();
+  String getMapVersion();
+  Map<String, IBuilding> getBuildings();
+  List<INode> getNodes();
+  List<IEdge> getEdges();
 }
 
 abstract class IBuilding {
   IBuilding(List<IFloor> floors);
-  Map<int, String> getNodes();
-  Map<int, String> getNodesByCategory(int cat);
+
+  List<IFloor> getFloors();
+  Map<String, int> getNodeIds();
+  Map<String, int> getNodesIdsByCategory(int cat);
 }
 
 abstract class IFloor {
   IFloor(int level, List<int> nids);
+
+  int getLevel();
+  List<int> getNodeIds();
 }
 
 abstract class INode {
   INode(String name, String buildingName, int fid, int x, int y, int cat, List<int> eids, Map<String, dynamic> add);
+
+  String getName();
+  String getBuildingName();
+  int getFloorId();
+  (int, int) getCoords();
+  int getCategory();
+  List<int> getEdgeIds();
+  Map<String, dynamic> getAdditional();
 }
 
 abstract class IEdge {
-  IEdge(int nid1, int nid2, double distance, Map<String, dynamic> add);
+  IEdge(int nid1, int nid2, double dist, Map<String, dynamic> add);
+
+  (int, int) getNodeIds();
+  double getDistance();
+  Map<String, dynamic> getAdditional();
 }
